138bff3954b2c6d313b8c99a7c66f10dccfc5442
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-12 22:06:19 +0600

    Update 7-mimumSumLeetCode2160.js

--- algorithoms/7-mimumSumLeetCode2160.js
+++ algorithoms/7-mimumSumLeetCode2160.js
@@ -6,9 +6,21 @@ function minimumSum(num) {
     let arr = num.toString().split("").sort()
     console.log('sorted array:', arr)
     const sum = Number(arr[0] + arr[3]) + Number(arr[1] + arr[2])
-    console.log("minimum sum :",sum)
+    console.log("minimum sum :", sum)
 }
 minimumSum(4009)
+
+// practice again 
+
+function minimumSumOfFourDigit(number) {
+    let array = number.toString().split("").sort()
+    console.log("sorted array of numbers: ", array)
+    let minimumSum = Number(array[0] + array[2]) + Number(array[1] + array[3])
+    console.log("Minimum Sum of the Four Digit:", minimumSum)
+
+}
+
+minimumSumOfFourDigit(4004)
 
 
 
