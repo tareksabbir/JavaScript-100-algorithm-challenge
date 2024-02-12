e3ceb0f06896313dfe7a78d298d8486028088000
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-12 20:13:56 +0600

    Update 6-Patterns.js

--- algorithoms/6-Patterns.js
+++ algorithoms/6-Patterns.js
@@ -16,6 +16,56 @@ function rectangularPattern() {
 
 rectangularPattern()
 
+//hollow rectangle pattern
+
+function hollowRectangle() {
+
+    let row = 5
+    let col = 5
+    let stars = ''
+    for (let i = 1; i <= row; i++) {
+        for (let j = 1; j <= col; j++) {
+            if (j == 1 || j == col) {
+                stars += ' * '
+            } else if (i == 1 || i == row) {
+                stars += " * "
+            } else {
+                stars += "   "
+            }
+        }
+        stars += '\n'
+    }
+    //console.log(stars)
+}
+
+hollowRectangle()
+
+
+function hollowRectanglePattern() {
+    let row = 5
+    let col = 5
+    let stars = ''
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (j == 0 || j == col - 1) {
+                stars += " * "
+            }
+            else if (i == 0 || i == row - 1) {
+                stars += " * "
+            } else {
+                stars += "   "
+            }
+
+        }
+        stars += "\n"
+    }
+    console.log(stars)
+
+}
+
+hollowRectanglePattern()
+
+
 
 
 
