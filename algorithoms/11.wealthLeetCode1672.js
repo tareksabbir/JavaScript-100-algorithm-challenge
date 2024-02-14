70320b0c796169ff89cd0cb28bf5bde275d05d96
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-15 03:57:20 +0600

    Update 11.wealthLeetCode1672.js

--- algorithoms/11.wealthLeetCode1672.js
+++ algorithoms/11.wealthLeetCode1672.js
@@ -45,3 +45,28 @@ function maximumWealth(accounts) {
 }
 
 console.log(maximumWealth([[2, 8, 7], [7, 1, 3], [1, 9, 5]]))
+
+
+/// approach 2 
+
+function maximumWealth2(accounts) {
+    let rich = []
+    for (let i = 0; i < accounts.length; i++) {
+        let sum = 0
+        for (let j = 0; j < accounts[i].length; j++) {
+            sum += accounts[i][j]
+        }
+        rich.push(sum)
+
+    }
+    
+    if (Math.max(...rich) === Math.min(...rich)) {
+        return Math.max(...rich)
+    } else {
+        return Math.max(...rich)
+    }
+
+
+}
+
+maximumWealth2([[2, 8, 7], [7, 1, 3], [1, 9, 5]])
