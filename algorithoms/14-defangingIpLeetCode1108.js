e4b367ded7bfea76590120ec96f91f0e9a8a10d5
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-15 07:11:09 +0600

    Create 14-defangingIpLeetCode1108.js

---
+++ algorithoms/14-defangingIpLeetCode1108.js
@@ -0,0 +1,48 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+
+var defangIPaddr = function (address) {
+    let newIp = ''
+    let str = address.split(".")
+    let length=str.length
+    for (let i=0;i<length;i++) {
+        newIp = newIp + str[i] + "[.]"
+    }
+    return newIp.slice(0,newIp.length-3)
+
+
+};
+defangIPaddr("255.100.50.0")
+
+//way 2
+
+
+var defangIPaddr2 = function (address) {
+   
+    let str = address.split(".")
+    let length= str.length
+    for (let i=0;i<length;i++) {
+        if(str[i]==='.'){
+            str[i]='[.]'
+        }
+        
+    }
+    console.log(str)
+    
+
+
+};
+defangIPaddr2("255.100.50.0")
