56117a75d88d46a32c4494e0e5029ae490723959
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-17 04:18:13 +0600

    Create 25-objectInToAnArray.js

---
+++ algorithoms/25-objectInToAnArray.js
@@ -0,0 +1,21 @@
+// function that converts an object into an array where each element represented a key-value pair from an array of object 
+
+function objectToAnArray(obj) {
+    let array = Array.from(Object.entries(obj))
+    console.log(array)
+
+
+}
+
+const genderObjs = {
+    "-3": { m: "great grandFather", f: "great grandmother" },
+    "-2": { m: "grandFather", f: "grandmother" },
+    "-1": { m: "Father", f: "Mother" },
+    "0": { m: "me", f: "me" },
+    "1": { m: "son", f: "daughter" },
+    "2": { m: "grand son", f: "grand daughter" },
+    "3": { m: "great grand son", f: "great grand daughter" },
+
+}
+
+objectToAnArray(genderObjs)
