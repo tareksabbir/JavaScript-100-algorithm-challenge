eabd738d2648f6460d2fc8269c3cf855a461fe15
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-16 09:27:15 +0600

    Create leetCode2632.js

---
+++ LeetCode JS 30-Day Challenge/leetCode2632.js
@@ -0,0 +1,12 @@
+// given a function fn, return a curried version of that function 
+
+var curry = function (fn) {
+    return function curried() {
+
+    }
+}
+
+
+// function sum(a, b) { return a + b }
+// let csum = curry(sum)
+// csum(1)(2)
