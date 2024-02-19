b4f0d59e86d10db07c477dd87ea0b6d6438552bb
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-20 05:49:48 +0600

    Update 40-countStonesLeetCode771.js

--- algorithoms/40-countStonesLeetCode771.js
+++ algorithoms/40-countStonesLeetCode771.js
@@ -20,8 +20,8 @@ var numJewelsInStones = function (jewels, stones) {
     let count = 0
     for (let i = 0; i < jewels.length; i++) {
         for (let j = 0; j < stones.length; j++) {
-            if (jewels[i] === stones[j]){
-                 count+=1
+            if (jewels[i] === stones[j]) {
+                count += 1
             }
         }
     }
