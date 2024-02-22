34f0b3175c5435a7832839054ce5b2d3491ab25c
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-22 11:06:55 +0600

    Update removeElement.js

--- data-structure/Binary Search/removeElement.js
+++ data-structure/Binary Search/removeElement.js
@@ -2,14 +2,34 @@ let data = [3, 2, 2, 3]
 
 
 function removingElement(nums, val) {
-    let j =0
-    for(let n of nums){
-        if(n!==val){
-            nums[++j]=n
+    let j = 0
+    for (let n of nums) {
+        if (n !== val) {
+            nums[++j] = n
         }
     }
-    return j 
-    
+    return j
+
 }
 
 console.log(removingElement(data, 3))
+
+
+//way 2
+
+
+var removeElement2 = function (nums, val) {
+    var a = []
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            a.push(nums[i])
+        }
+    }
+    for (var i = 0; i < a.length; i++) {
+        nums[i] = a[i]
+    }
+    return a.length
+};
+
+
+removeElement2([2, 3, 3, 3, 4, 5], 3)
