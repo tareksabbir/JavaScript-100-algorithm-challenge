24dee4e10aed6b6a74a583b26d6703c8467ac241
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-23 04:42:24 +0600

    Update 45-singleNumberLeetCode136.js

--- algorithoms/45-singleNumberLeetCode136.js
+++ algorithoms/45-singleNumberLeetCode136.js
@@ -3,7 +3,7 @@ function singleNumber(nums) {
     for (let i of nums) {
         map[i] ? map[i]++ : map[i] = 1
     }
-    
+    console.log(map)
     for (let key in map) {
         if (map[key] === 1) {
           return Number(key)
