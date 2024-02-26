19814d7fc56eeff47169414558fb0cd9bd0e511a
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-27 05:50:30 +0600

    Update practice.js

--- data-structure/LinkList/practice.js
+++ data-structure/LinkList/practice.js
@@ -1,12 +1,31 @@
-class Node{
-    constructor(data){
+class Node {
+    constructor(data) {
         this.data = data
         this.next = null
     }
 }
 
-class LinkList{
-    constructor(){
-        this.head = null 
+class LinkList {
+    constructor() {
+        this.head = null
+    }
+    addFirstNode(data) {
+        let newNode = new Node(data)
+        newNode.next = this.head
+        this.head = newNode
+    }
+    print(){
+        let current = this.head
+        while(current){
+            console.log(current.data)
+            current = current.next
+        }
     }
 }
+
+
+let link = new LinkList()
+
+link.addFirstNode(2)
+link.addFirstNode(3)
+link.print()
