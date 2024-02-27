7ac4a90a479c18f7bc8f1087ab9ca6d156885868
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-27 08:46:35 +0600

    Update practice.js

--- data-structure/LinkList/practice.js
+++ data-structure/LinkList/practice.js
@@ -9,23 +9,63 @@ class LinkList {
     constructor() {
         this.head = null
     }
-    addFirstNode(data) {
-        let newNode = new Node(data)
+    addNode(data) {
+        const newNode = new Node(data)
         newNode.next = this.head
         this.head = newNode
     }
-    print(){
+    addNodeLast(data) {
+        const newNode = new Node(data)
+        if (!this.head) {
+            newNode.next = this.head
+            return
+        }
         let current = this.head
-        while(current){
+        while (current.next) {
+            current = current.next
+        }
+        current.next = newNode
+    }
+    size() {
+        let count = 0
+        let current = this.head
+        while (current) {
+            count++
+            current = current.next
+        }
+        return count
+    }
+    addAtAnyIndex(index, data) {
+        if (index < 0 || index > this.size()) {
+            console.log('invalid index')
+        }
+        let newNode = new Node(data)
+        if (index === 0) {
+            newNode.next = this.head
+            this.head = newNode
+            return
+        }
+        let current = this.head
+        for (let i = 0; i < index - 1; i++) {
+            current = current.next
+        }
+        newNode.next = current.next
+        current.next = newNode
+    }
+    print() {
+        let current = this.head
+        while (current) {
             console.log(current.data)
             current = current.next
         }
     }
 }
 
+let list1 = new LinkList()
+list1.addNode(10)
+list1.addNode(20)
+list1.addNode(30)
+list1.addNodeLast(40)
+list1.addAtAnyIndex(2, 50)
+list1.print()
 
-let link = new LinkList()
-
-link.addFirstNode(2)
-link.addFirstNode(3)
-link.print()
