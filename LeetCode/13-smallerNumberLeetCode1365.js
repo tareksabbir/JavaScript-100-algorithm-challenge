2378f7771294130ac7ae31961dc24862b2fb62e1
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-28 09:14:37 +0600

    Create 13-smallerNumberLeetCode1365.js

---
+++ LeetCode/13-smallerNumberLeetCode1365.js
@@ -0,0 +1,57 @@
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+// Return the answer in an array.
+
+
+// Example 1:
+
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation: 
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1). 
+// For nums[3]=2 there exist one smaller number than it (1). 
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+// Example 2:
+
+// Input: nums = [6,5,4,8]
+// Output: [2,1,0,3]
+// Example 3:
+
+// Input: nums = [7,7,7,7]
+// Output: [0,0,0,0]
+
+
+var smallerNumbersThanCurrent = function (nums) {
+    let arr = []
+    let numsLength = nums.length
+    for (let i = 0; i < numsLength; i++) {
+        let count = 0
+        for (let j = 0; j < numsLength; j++) {
+            if (nums[j] < nums[i] && j !== i) {
+                count++
+            }
+        }
+        arr[i] = count
+    }
+    return arr
+};
+
+
+smallerNumbersThanCurrent([7, 7, 7, 7])
+
+var smallerNumbersThanCurrent2 = function (nums) {
+    let arr = []
+    for (let n of nums) {
+        let count = 0
+        for (let m of nums) {
+            if (n > m && n !== m) count++
+        }
+        arr.push(count)
+    }
+    return arr
+};
+
+
+smallerNumbersThanCurrent2([7, 7, 7, 7])
