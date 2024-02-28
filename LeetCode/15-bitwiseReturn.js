c87b1ed42136ae191583cdb0e6a4a4a171200a5d
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-28 09:14:44 +0600

    Create 15-bitwiseReturn.js

---
+++ LeetCode/15-bitwiseReturn.js
@@ -0,0 +1,7 @@
+const bitWiseAnd = (num1, num2) => num1 & num2
+const bitWiseOr = (num1, num2) => num1 | num2
+const bitWiseXor = (num1, num2) => num1 ^ num2
+
+console.log(bitWiseAnd(6,23))
+console.log(bitWiseOr(6,23))
+console.log(bitWiseXor(6,23))
