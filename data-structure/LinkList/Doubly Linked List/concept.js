f5a44c291d445b66d7bab394884dee449a235e0e
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-28 08:52:57 +0600

    Update concept.js

--- data-structure/LinkList/Doubly Linked List/concept.js
+++ data-structure/LinkList/Doubly Linked List/concept.js
@@ -1,17 +1,25 @@
 // doubly linked list 
 
-class Node{
-    constructor(data){
+class Node {
+    constructor(data) {
         this.data = data
         this.prev = null
         this.next = null
     }
 }
 
-class DoublyLinkedList{
-    constructor(){
+class DoublyLinkedList {
+    constructor() {
         this.head = null
     }
 
+    addToTheTop(data) {
+        const newNode = new Node(data)
+        newNode.next = this.head
+        if (this.head) {
+            this.head.prev = newNode
+        }
+        this.head = newNode
+    }
 
 }
