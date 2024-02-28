c1f16fb350f9abdfbadf9e2c6ba5818c6ff4f8aa
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-28 09:14:46 +0600

    Create 16-addUpNumbers.js

---
+++ LeetCode/16-addUpNumbers.js
@@ -0,0 +1,11 @@
+// add up the numbers from a single Numbers
+
+function addUpTheNumber(nums){
+    let sum =0
+    for(let i=0;i<=nums;i++){
+        sum+=i
+    }
+    console.log(sum)
+
+}
+addUpTheNumber(11)
