110c877ddf32d5b29f0147074b1bba5b8ff38e16
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-28 09:14:55 +0600

    Create 2-sumOfAllParameters.js

---
+++ LeetCode/2-sumOfAllParameters.js
@@ -0,0 +1,39 @@
+// write a function that returns the sum of two numbers 
+// write a function that returns the sum of all numbers regardless of # params 
+// hint arithmetic operators and restOperators and forEach()
+
+// way one 
+function sumOfAllParams(...params) {
+    return params.reduce((total, value) => total + value,0)
+}
+
+let result = sumOfAllParams(1, 2, 3, 4, 5, 6)
+
+console.log('way one: ', result)
+
+// way two
+
+function sumOfAllParams2(...params) {
+    let sum = 0
+    for (let value of params) {
+        sum = sum + value
+    }
+    console.log('way two: ', sum)
+
+}
+
+sumOfAllParams2(10, 20, 30, 40, 50)
+
+
+// way three
+
+function sumOfAllParams3(...params) {
+    let sumSoFar = 0
+    params.forEach((value) => {
+        sumSoFar += value
+    })
+    console.log('way three: ',sumSoFar)
+
+}
+
+sumOfAllParams3(100,200,300)
