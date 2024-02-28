b1a96deecdc5fac7e116dcf698c868b7abc0f9e0
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-28 09:15:00 +0600

    Create 21-twoInputButSpecificOutcome.js

---
+++ LeetCode/21-twoInputButSpecificOutcome.js
@@ -0,0 +1,16 @@
+// two input a function but it will return specific number
+
+//ex: if a+b = 10  it will return true
+//
+
+const checkTen = (x, y) => {
+    return ((x + y === 10)||x===10||y===10) ? true : false
+}
+
+let result = checkTen(10, 8)
+console.log(result)
+
+
+
+
+
