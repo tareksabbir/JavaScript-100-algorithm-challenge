3d4e1cf8a32ec6ccb30b7307a5afa0420ecd7ef0
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-02-28 09:12:27 +0600

    Delete 15-bitwiseReturn.js

--- algorithoms/15-bitwiseReturn.js
+++
@@ -1,7 +0,0 @@
-const bitWiseAnd = (num1, num2) => num1 & num2
-const bitWiseOr = (num1, num2) => num1 | num2
-const bitWiseXor = (num1, num2) => num1 ^ num2
-
-console.log(bitWiseAnd(6,23))
-console.log(bitWiseOr(6,23))
-console.log(bitWiseXor(6,23))
