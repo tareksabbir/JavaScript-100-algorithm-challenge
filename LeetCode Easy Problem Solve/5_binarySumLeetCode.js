a9c539c77e565d8df45b6bbc79ed7b871275de3e
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-02 19:48:27 +0600

    Update 5_binarySumLeetCode.js

--- LeetCode Easy Problem Solve/5_binarySumLeetCode.js
+++ LeetCode Easy Problem Solve/5_binarySumLeetCode.js
@@ -1,17 +1,27 @@
 
-function binary_Sum(a,b){
-    return (BigInt(`0b${a}`)+BigInt(`0b${b}`)).toString(2)
+function binary_Sum(a, b) {
+    return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2)
 }
 
-console.log(binary_Sum(10,1))
+console.log(binary_Sum(10, 1))
 
 
 // WAY 2 
 
-var addBinary = function(a, b) {
+var addBinary = function (a, b) {
     let s1 = BigInt('0b' + a);
     let s2 = BigInt('0b' + b);
-    return (s1+s2).toString(2)
+    return (s1 + s2).toString(2)
 };
 
-addBinary(9,9)
+addBinary(9, 9)
+
+
+
+// sqrt(x) leet code 69
+
+function sqrt(x) {
+    return Math.pow(x, 2)
+
+}
+console.log(sqrt(10))
