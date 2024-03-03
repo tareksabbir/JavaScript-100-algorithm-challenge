00506ee74d31e1fcaf04e375c2e7571c18c1a36d
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-03 16:31:16 +0600

    Update Day_2LeetCode19.js

--- Daily Challanges/Day_2LeetCode19.js
+++ Daily Challanges/Day_2LeetCode19.js
@@ -37,3 +37,29 @@ var removeNthFromEnd = function(head, n) {
     const pos=removeNode(head,n)
     return pos==n?head.next:head
 };
+
+
+//way 2 best approach
+
+var removeNthFromEnd2 = function(head, n) {
+    let dummy = new ListNode()
+    dummy.next = head
+
+    let firstPointer = dummy
+    let secondPointer = dummy
+
+    // Move the second pointer ahead by n times
+    for(let i = 0; i < n; i++){
+        secondPointer = secondPointer.next
+    }
+    // After moving the second pointer till it's one ahead of n node, now lets start moving both pointers
+    while(secondPointer.next){
+        firstPointer = firstPointer.next
+        secondPointer = secondPointer.next
+    }
+
+    //  Now if second's next hits null, we move the first to bypass its next
+    firstPointer.next = firstPointer.next.next
+
+    return dummy.next
+};
