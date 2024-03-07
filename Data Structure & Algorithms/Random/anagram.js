dcf1e712e58487e7161e863adaa6d9cd5a0c4ebe
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-07 13:15:24 +0600

    Update anagram.js

--- Data Structure & Algorithms/Random/anagram.js
+++ Data Structure & Algorithms/Random/anagram.js
@@ -18,18 +18,18 @@ function anagrams(strOne, strTwo) {
     let countStr1 = countStringsNew(strOne)
     let countStr2 = countStringsNew(strTwo)
 
-    if(Object.keys(countStr1).length !== Object.keys( countStr2).length){
+    if (Object.keys(countStr1).length !== Object.keys(countStr2).length) {
         return false
-    } 
+    }
 
-    for(let key in countStr1){
-        if (countStr1[key] !== countStr2[key]){
+    for (let key in countStr1) {
+        if (countStr1[key] !== countStr2[key]) {
             return false
         }
     }
 
     return true
-    
+
 
 
 }
@@ -38,22 +38,43 @@ function anagrams(strOne, strTwo) {
 
 
 
-function sorting(str){
-    return str.replace(/[^\w]/g,'').split('').sort().join('')
+function sorting(str) {
+    return str.replace(/[^\w]/g, '').split('').sort().join('')
 
 }
 
-function anagramFindBySorting(stringOne,stringTow){
-    const  sortedStringOne = sorting(stringOne)
-    const  sortedStringTwo = sorting(stringTow)
-    
-    if(sortedStringOne===sortedStringTwo){
+function anagramFindBySorting(stringOne, stringTow) {
+    const sortedStringOne = sorting(stringOne)
+    const sortedStringTwo = sorting(stringTow)
+
+    if (sortedStringOne === sortedStringTwo) {
         console.log('it is an anagram')
-    }else{
+    } else {
         console.log('it is not an anagram')
     }
 
 }
 
-anagramFindBySorting("bad word","dab wor")
+anagramFindBySorting("bad word", "dab wor")
 
+
+// way 3 
+function map (str){
+    let result = new Map()
+    str.split('').forEach(element => {
+        result.set(element, result.get(element) ? result[element] + 1 : 1)
+    });
+    return result
+}
+
+function anagramMap(s, t) {
+    let map1 = map(s)
+    let map2 = map(t)
+    if(map1.size!==map2.size) return false
+    for(let n of map1){
+        if(map1[n]!=map2[n]) return false
+    }
+    return true
+}
+
+console.log(anagramMap("tarek", "kerat"))
