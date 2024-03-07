86810dc0ea57534de8880544ea8647f462401702
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-07 13:23:59 +0600

    Update anagram.js

--- Data Structure & Algorithms/Random/anagram.js
+++ Data Structure & Algorithms/Random/anagram.js
@@ -59,7 +59,7 @@ anagramFindBySorting("bad word", "dab wor")
 
 
 // way 3 
-function map (str){
+function map(str) {
     let result = new Map()
     str.split('').forEach(element => {
         result.set(element, result.get(element) ? result[element] + 1 : 1)
@@ -70,11 +70,23 @@ function map (str){
 function anagramMap(s, t) {
     let map1 = map(s)
     let map2 = map(t)
-    if(map1.size!==map2.size) return false
-    for(let n of map1){
-        if(map1[n]!=map2[n]) return false
+    if (map1.size !== map2.size) return false
+    for (let n of map1) {
+        if (map1[n] != map2[n]) return false
     }
     return true
 }
 
-console.log(anagramMap("tarek", "kerat"))
+//console.log(anagramMap("tarek", "kerat"))
+
+
+
+//easy solution 
+
+
+function easyAnagram(s, t) {
+    if (s.length != t.length) return false
+    return s.split("").sort().join("") === t.split("").sort().join("")
+}
+
+console.log(easyAnagram("rat", "amte"))
