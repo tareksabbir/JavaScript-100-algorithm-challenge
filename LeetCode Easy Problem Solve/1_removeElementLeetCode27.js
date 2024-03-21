8aef74a404c463c6236cad909a46ea8db754adb7
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-22 03:37:20 +0600

    Update 1_removeElementLeetCode27.js

--- LeetCode Easy Problem Solve/1_removeElementLeetCode27.js
+++ LeetCode Easy Problem Solve/1_removeElementLeetCode27.js
@@ -1,15 +1,7 @@
 /*
 
-Code
-Testcase
-Test Result
-Test Result
 27. Remove Element
-Solved
-Easy
-Topics
-Companies
-Hint
+
 Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
