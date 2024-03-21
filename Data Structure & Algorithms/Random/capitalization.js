fb7e064a4d2857519e04c30f60dd7d89205200fa
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-22 03:02:26 +0600

    Update capitalization.js

--- Data Structure & Algorithms/Random/capitalization.js
+++ Data Structure & Algorithms/Random/capitalization.js
@@ -3,11 +3,25 @@ function capitalization(string) {
     for (let value of string.split(" ")) {
         arr.push(value[0].toUpperCase() + value.slice(1))
     }
-    return arr.join(" ")
+    return arr.join(' ')
 
 
 }
 console.log(capitalization("hello mother fucker how are you ?"))
+
+
+
+
+function cap(str) {
+    let arr = []
+    for (let n of str.split(' ')) {
+        arr.push(n[0].toUpperCase() + n.slice(1))
+    }
+    console.log(arr.join(" "))
+
+}
+
+cap("hello how are you ?")
 
 
 
