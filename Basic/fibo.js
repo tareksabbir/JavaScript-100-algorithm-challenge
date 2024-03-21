c30a2b591eda80358c1562012c534dc4537a26e4
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-22 01:48:49 +0600

    Create fibo.js

---
+++ Basic/fibo.js
@@ -0,0 +1,21 @@
+function fibo(n) {
+    if (n <= 1) return n
+    return fibo(n - 1) + fibo(n - 2)
+
+}
+
+console.log(fibo(10))
+console.log(fibo(12))
+
+
+function fibo2(n) {
+    if (n <= 1) return n
+    let arr = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        arr.push(arr[i-1]+arr[i-2])
+    }
+    console.log(arr)
+    console.log(arr[n])
+}
+
+fibo2(10)
