521eafef100261b40c9e06045a50b5748f3f46de
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-23 02:48:55 +0600

    Update 59_romanToIntegerLeetCode13.js

--- LeetCode Easy Problem Solve/59_romanToIntegerLeetCode13.js
+++ LeetCode Easy Problem Solve/59_romanToIntegerLeetCode13.js
@@ -2,23 +2,24 @@
 // for example I = 1 , v= r ,x = 10
 
 function romanToInt(s) {
-    let roman = { I: 1, V: 5, X: 10, L: 50, C: 10, D: 500, M: 1000 }
-    let sum = 0
-    let string = s.toUpperCase().split('')
-    let arraylength = string.length
+    let roman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let sum = 0;
+    let string = s.toUpperCase().split('');
+    let arraylength = string.length;
 
     for (let i = 0; i < arraylength; i++) {
-        let currentValue = string[i]
-        let nextValue = string[i + 1]
-        if (roman[currentValue] < roman[nextValue]) {
-            sum = sum + (roman[nextValue] - roman[currentValue])
-            i++
+        let currentValue = roman[string[i]];
+        let nextValue = roman[string[i + 1]];
+
+        if (nextValue && currentValue < nextValue) {
+            sum += nextValue - currentValue;
+            i++; 
         } else {
-            sum = sum + roman[currentValue]
+            sum += currentValue;
         }
     }
 
-    return sum
+    return sum;
 
 
 }
