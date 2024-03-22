9ac48704fb9924921c7c55c53be856be8f41e683
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-23 01:53:23 +0600

    Update 56_Patterns.js

--- LeetCode Easy Problem Solve/56_Patterns.js
+++ LeetCode Easy Problem Solve/56_Patterns.js
@@ -57,3 +57,54 @@ reversePattern(5)
 
 
 
+
+
+
+function makePattern(rows, col) {
+    let stars = ''
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < col; j++) {
+            stars += ' * '
+        }
+        stars += '\n'
+    }
+    return stars
+
+}
+
+console.log(makePattern(6, 6))
+
+
+function hollowRectangle(row, col) {
+    let star = ''
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (j == 0 || j == col - 1) {
+                star += ' * '
+            }
+            else if (i == 0 || i == row - 1) {
+                star += ' * '
+            } else {
+                star += '   '
+            }
+
+        }
+        star += '\n'
+    }
+    console.log(star)
+}
+
+hollowRectangle(6, 6)
+
+
+
+function reverseMethod(n) {
+    let start = ''
+    for (let i = n; i >= 1; i--) {
+        start = start + ' * '.repeat(i) + '\n'
+
+    }
+    console.log(start)
+
+}
+reverseMethod(6)
