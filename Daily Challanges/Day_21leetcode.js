b02c5b158008ce4f99e4a878d5fbadf7c71d188e
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-03-25 00:40:24 +0600

    Create Day_21leetcode.js

---
+++ Daily Challanges/Day_21leetcode.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let slow = nums[0];
+   for (let i = 0; i < nums.length; i++) {
+       let ind = Math.abs(nums[i]);
+       if (nums[ind] < 0) {
+           return ind;
+       }
+       nums[ind] = -nums[ind];
+   }
+   return -1;
+};
