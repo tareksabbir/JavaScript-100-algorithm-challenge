94b319c1a774e35174f4ee5b89685737ba5089fe
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-04-15 05:38:37 +0600

    Create Day_29LeetCode.js

---
+++ Daily Challanges/Day_29LeetCode.js
@@ -0,0 +1,12 @@
+function sumOfLeftLeaves(root) {
+    
+    function traverse(node, isLeft) {
+        if (node === null) return 0;
+        
+        if (!node.left && !node.right) return isLeft ? node.val : 0;
+        
+        return traverse(node.left, true) + traverse(node.right, false);
+    }
+    
+    return traverse(root, false);
+}
