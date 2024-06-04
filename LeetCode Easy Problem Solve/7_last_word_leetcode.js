910863863a60ef591f2ef9ac9a2df7bf79a59215
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-06-04 20:35:59 +0600

    Update 7_last_word_leetcode.js

--- LeetCode Easy Problem Solve/7_last_word_leetcode.js
+++ LeetCode Easy Problem Solve/7_last_word_leetcode.js
@@ -1,9 +1,9 @@
 function lengthOfLastNumber(str){
 
-    let strSplit = str.toString().split(" ")
+    let strSplit = str.toString().trim().split(" ")
     let strNew= strSplit[strSplit.length -1]
     return strNew.length
 }
 
-let arr =["hi sabbir how are you man?"]
+let arr =["hi sabbir how are you man? "]
 console.log(lengthOfLastNumber(arr))
