48878431d82585853b6cef073cd0bc6bcd4cf5c5
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-01 08:58:35 +0600

    Update reverseWord.js

--- Basic/reverseWord.js
+++ Basic/reverseWord.js
@@ -7,4 +7,14 @@ const reversed =(str)=>{
 }
 
 let result = reversed("hey man how are you ?")
-console.log (result)
+//console.log (result)
+
+
+// basic approach with out using function
+
+let str = "tumi mor jiboner vabona "
+
+let reverseArrayOfString= str.split(" ").map((e)=>{
+    return e.split("").reverse().join("")
+})
+console.log(reverseArrayOfString.join(" "))
