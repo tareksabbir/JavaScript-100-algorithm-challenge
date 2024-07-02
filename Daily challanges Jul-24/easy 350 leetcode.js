64ac5afd0a438294b64df514b90c36ffe80f8403
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-03 05:20:18 +0600

    Update easy 350 leetcode.js

--- Daily challanges Jul-24/easy 350 leetcode.js
+++ Daily challanges Jul-24/easy 350 leetcode.js
@@ -15,7 +15,35 @@ const intersection=(nums1,nums2)=>{
    return result
 
 }
-console.log(intersection([4,9,5],[9,4,9,8,4]))
+//console.log(intersection([4,9,5],[9,4,9,8,4]))
 
 
 
+// way 2 
+
+var intersect2 = function(nums1, nums2) {
+    nums1 = nums1.sort((a,b)=>a-b);
+    nums2 = nums2.sort((a,b)=>a-b);
+    
+    let i = 0 
+    let j = 0
+    let returnArray = [];
+    
+    while(j < nums2.length && i < nums1.length){
+        if(nums1[i] === nums2[j]){
+            returnArray.push(nums1[i]);
+            i++;
+            j++;
+        }
+        else if(nums1[i] > nums2[j]){
+            j++;
+        }
+        else{
+            i++;
+        }
+    }
+    
+    return returnArray;
+};
+
+console.log(intersect2([4,9,5],[9,4,9,8,4]))
