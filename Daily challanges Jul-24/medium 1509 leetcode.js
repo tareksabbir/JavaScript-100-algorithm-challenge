ce1cf94556383a04a2e8ba9fbd527b2d1963d1d4
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-04 05:34:14 +0600

    Create medium 1509 leetcode.js

---
+++ Daily challanges Jul-24/medium 1509 leetcode.js
@@ -0,0 +1,16 @@
+let minimumDifference = (nums) => {
+    if(nums.length <=4){
+        return 0
+    }
+    let arr = nums.sort((a, b) => a - b)
+    let n = arr.length
+    let result = []
+    result.push(arr[n-4]-arr[0])
+    result.push(arr[n-3]-arr[1])
+    result.push(arr[n-2]-arr[2])
+    result.push(arr[n-1]-arr[3])
+    return Math.max(...result)
+
+}
+
+minimumDifference([3, 2, 4, 5, 1])
