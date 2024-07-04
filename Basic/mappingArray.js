7cda5e6feac9475b075347f4987a9f1eac2bf4a6
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-05 04:48:50 +0600

    Update mappingArray.js

--- Basic/mappingArray.js
+++ Basic/mappingArray.js
@@ -1,12 +1,24 @@
-let mappingArray =(arr)=>{
+let mappingArray = (arr) => {
     let map = new Map()
     arr.forEach(e => {
-        if(map.has(e)){
-            map.set(e,map.get(e)+1)
-        } else{
-            map.set(e,1)
+        if (map.has(e)) {
+            map.set(e, map.get(e) + 1)
+        } else {
+            map.set(e, 1)
         }
     });
     console.log(map)
 }
-mappingArray([1,2,3,1,4,3,5,6,5,4,3])
+mappingArray([1, 2, 3, 1, 4, 3, 5, 6, 5, 4, 3])
+
+
+// way 2 
+
+let mappingArray2 = (arr) => {
+    let map = new Map()
+    for (let e of arr) {
+        map.set(e, (map.get(e) || 0) + 1)
+    }
+    return map
+}
+console.log(mappingArray2([1, 2, 3, 1, 4, 3, 5, 6, 5, 4, 3]))
