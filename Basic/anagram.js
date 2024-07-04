01191d7b9f2545e162d2f1dde1cc9d706abb6344
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-05 04:48:55 +0600

    Update anagram.js

--- Basic/anagram.js
+++ Basic/anagram.js
@@ -1,24 +1,34 @@
-function countChar(str) {
+// function countChar(str) {
+//     let map = new Map()
+//     for (let n of str) {
+//         if (!map.has(n)) {
+//             map.set(n, 1)
+//         } else {
+//             map.set(n, map.get(n) + 1)
+//         }
+//     }
+//     return map
+// }
+
+function countChar(str){
     let map = new Map()
-    for (let n of str) {
-        if (!map.has(n)) {
-            map.set(n, 1)
-        } else {
-            map.set(n, map.get(n) + 1)
-        }
+    for(let e of str){
+        map.set(e,(map.get(e)||0) +1  )
     }
-    return map
+    console.log(map)
 }
 
-function anagram(str1, str2) {
-    if (str1.length !== str2.length) return false
-    let s1 = countChar(str1)
-    let s2 = countChar(str2)
-    for (let n of s1) {
-        if (s1[n] !== s2[n]) return false
-    }
-    return true
-}
+countChar("aabbeemmw")
+
+// function anagram(str1, str2) {
+//     if (str1.length !== str2.length) return false
+//     let s1 = countChar(str1)
+//     let s2 = countChar(str2)
+//     for (let n of s1) {
+//         if (s1[n] !== s2[n]) return false
+//     }
+//     return true
+// }
 
 
 //console.log(anagram("carrace", "racecar"))
@@ -29,4 +39,4 @@ function anagram2(str1, str2) {
     return str1.split("").sort().join("") == str2.split("").sort().join("")
 }
 
-console.log(anagram2('care', 'race'))
+//console.log(anagram2('care', 'race'))
