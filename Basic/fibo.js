eef94b59c5aa2e0eebbcf4bdd01de27dc5a5ac22
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-05 05:23:11 +0600

    Update fibo.js

--- Basic/fibo.js
+++ Basic/fibo.js
@@ -12,10 +12,30 @@ function fibo2(n) {
     if (n <= 1) return n
     let arr = [0, 1]
     for (let i = 2; i <= n; i++) {
-        arr.push(arr[i-1]+arr[i-2])
+        arr.push(arr[i - 1] + arr[i - 2])
     }
     console.log(arr)
     console.log(arr[n])
 }
 
 fibo2(10)
+
+
+function fibo3(n) {
+    if (n <= 1) return n
+    return fibo3(n - 1) + fibo3(n - 2)
+}
+
+console.log("fibo 3: ", fibo3(10))
+
+
+const fibo4 = (n) => {
+    if (n <= 1) return n
+    let arr = [0, 1]
+    for (let i =2 ;i<=n;i++) {
+        arr.push(arr[i - 1] + arr[i - 2])
+    }
+    console.log(arr)
+}
+
+fibo4(10)
