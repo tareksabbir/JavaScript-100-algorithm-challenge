e4d2af5effa8dbb15c4b0f3c84a0228650016477
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-08 05:25:35 +0600

    Create easy 1518 leetcode.js

---
+++ Daily challanges Jul-24/easy 1518 leetcode.js
@@ -0,0 +1,24 @@
+
+var numWaterBottles = function(numBottles, numExchange) {
+    let ans = numBottles;
+    while(numBottles >= numExchange){
+       let newBottles = Math.floor(numBottles / numExchange);
+       let remBottle = numBottles % numExchange;
+       ans = ans + newBottles ;
+       numBottles = newBottles + remBottle;
+    }
+    return ans;
+};
+
+ //way 2
+
+var numWaterBottles2 = function (numBottles, numExchange) {
+    let totalBottles = numBottles;
+
+    while (numBottles >= numExchange) {
+        totalBottles += Math.floor(numBottles / numExchange);
+        numBottles = Math.floor(numBottles / numExchange) + (numBottles % numExchange);
+    }
+
+    return totalBottles;
+};
