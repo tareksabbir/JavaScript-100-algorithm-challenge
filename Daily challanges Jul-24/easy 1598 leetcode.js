858189419d9fa530fd7c2335acc95923b382e4fe
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-07-10 08:54:27 +0600

    Update easy 1598 leetcode.js

--- Daily challanges Jul-24/easy 1598 leetcode.js
+++ Daily challanges Jul-24/easy 1598 leetcode.js
@@ -8,3 +8,18 @@ var minOperations = function (logs) {
     }
     return counter;
 };
+
+
+let folders = (logs) => {
+    let counter = 0
+    for (let log of logs) {
+        if (log == './') continue
+        if (log == '../') {
+            if (counter > 0) counter--
+
+        } else counter++
+    }
+    return counter
+}
+
+console.log(folders(["d1/","d2/","../","d21/","./"]))
