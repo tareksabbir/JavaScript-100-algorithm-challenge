c3a4fc959f965dabfed67dd7a37fe91af4a0a4e5
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-14 23:43:25 +0600

    updated

---
+++ Basic concept/random problems/bigNumber.js
@@ -0,0 +1,6 @@
+const bigNUmber = (arr)=>{
+    arr.sort((a,b)=>b-a)
+    console.log(arr[0])
+}
+
+bigNUmber([4,6,3,5,7,8,2,1,0,7,9,21])
--- Basic concept/random problems/palindrome.js
+++ Basic concept/random problems/palindrome.js
@@ -1,16 +1,36 @@
-function palindrome(str){
-    console.log(str.split("").reverse().join('') === str)
+// function palindrome(str){
+//     console.log(str.split("").reverse().join('') === str)
+// }
+// palindrome("abba")
+
+// function palindromeNumber(num){
+//     console.log(+num.toString().split("").reverse().join("") === num)
+
+// }
+// palindromeNumber(123321)
+
+
+// const palindrome2 =(str)=>{
+//     console.log(str.split("").reverse().join('') ===str)
+// }
+// palindrome2("abbaa")
+
+
+
+const palindromePractice30 =(str)=>{
+    let newStr  = str.split("").reverse().join("") == str
+    //console.log(newStr)
 }
-palindrome("abba")
 
-function palindromeNumber(num){
-    console.log(+num.toString().split("").reverse().join("") === num)
+palindromePractice30("abba")
+
+const numbersPalindrome  = (num)=>{
+    const numArray = +num.toString().split("").reverse().join("") ===num
+    console.log(numArray)
 
 }
-palindromeNumber(123321)
 
+numbersPalindrome(123321)
 
-const palindrome2 =(str)=>{
-    console.log(str.split("").reverse().join('') ===str)
-}
-palindrome2("abbaa")
+// IDEA: basically if we use + operator before any string of number value it will convert that number string into a number  
+
