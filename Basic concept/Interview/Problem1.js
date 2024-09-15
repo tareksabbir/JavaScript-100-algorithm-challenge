4531cde7449decfc642f63d055633851cd9b8f10
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-16 02:44:39 +0600

    comment update

--- Basic concept/Interview/Problem1.js
+++ Basic concept/Interview/Problem1.js
@@ -1,8 +1,11 @@
+//QS: what will be the output is ? undefind or new york
+//ANS: output will be newYork 
+
+
 let person = { name: "jon", city: "New York" }
 let department = {}
 department.student = person
 person = null
 console.log(department?.student?.city)
 
-//TODO: output will be newYork 
-//BUG: This is because the department object still holds a reference to the original person object, even though the person variable has been set to null. The student property of department still points to the original person object, and its city property is still "New York".
+//INFO: This is because the department object still holds a reference to the original person object, even though the person variable has been set to null. The student property of department still points to the original person object, and its city property is still "New York".
