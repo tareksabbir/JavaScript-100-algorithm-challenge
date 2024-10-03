0140aeb7d159116304b8020bde127f056c551168
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-10-03 10:48:05 +0600

    update question ans

---
+++ Basic concept/Interview/ConditionaRendering.js
@@ -0,0 +1,12 @@
+
+// ekhane jodi lang ta true hoi tahiley sudhu right side element ta output e dibe
+const lang = "LearnWithSumit";
+const result1 = lang && "JavaScript";
+console.log(result1); //JavaScript
+
+const lang2 = false;
+const result2 = lang2 && "JavaScript";
+
+console.log(result2); //false
+
+
