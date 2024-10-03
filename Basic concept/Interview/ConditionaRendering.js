a7fd6e8583f2ebd402e96cf4d20c4b9d83861fdd
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-10-03 10:51:00 +0600

    or operator question ans

--- Basic concept/Interview/ConditionaRendering.js
+++ Basic concept/Interview/ConditionaRendering.js
@@ -9,4 +9,8 @@ const result2 = lang2 && "JavaScript";
 
 console.log(result2); //false
 
+// or oparetor ekhane lang 3 jodi falsey value hoi jamon 0 , null , undifined , false etc.. thik tokhon e  right side value output dibe  otherwise lang3 e print korbe 
+const lang3 = "LearnWithSumit";
+const result3 = lang3 || "JavaScript";
 
+console.log(result3); // LearnWithSumit
