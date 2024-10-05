2e49f24fa9998c6f47f28d73ffedcbd7ee2ab31d
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-10-05 15:35:31 +0600

    Create 27. Remove Element.js

---
+++ Top Interview 150 LeetCode/27. Remove Element.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    // Pointer to track the position of the next valid element
+    let position = 0;
+
+    // Traverse through the array
+    for (let i = 0; i < nums.length; i++) {
+        // If the current element is not equal to the target value
+        if (nums[i] !== val) {
+            // Move it to the position pointer and increment the pointer
+            nums[position] = nums[i];
+            position++;
+        }
+    }
+
+    // The position now indicates the length of the modified array
+    return position;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3))
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
