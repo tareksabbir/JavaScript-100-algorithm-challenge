cde62779689db589b02ffebfca91ec19868f32d3
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-11-01 22:06:44 +0600

    Create 80. Remove Duplicates from Sorted Array II.js

---
+++ Leetcode Medium Problem Solve/80. Remove Duplicates from Sorted Array II.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    if (nums == []) return nums.length
+    for (let i = 0; i < nums.length; i++) {
+        let count = 1;
+        for (let j = i; j < nums.length - 1; j++) {
+            if (nums[i] == nums[j + 1]) {
+                count++;
+                if (count > 2) {
+                    nums.splice(j + 1, 1)
+                    j--
+                }
+            }
+
+        }
+    }
+    return nums.length
+};
