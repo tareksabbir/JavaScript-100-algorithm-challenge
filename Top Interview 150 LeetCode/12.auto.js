4dde3e3c536594f12f71446b3750775012112f8a
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-11-09 23:39:22 +0600

    Create 12.auto.js

---
+++ Top Interview 150 LeetCode/12.auto.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    let res = parseInt(s)
+  if (isNaN(res)) return 0;
+  if(res > Math.pow(2, 31)-1) {
+      return Math.pow(2, 31)-1;
+  } else if(res < -1*Math.pow(2, 31)) {
+      return -1*Math.pow(2, 31);
+  } else {
+      return res;
+  }
+};
